7c6a616f8ecb70a09d0381f81c1b36510649403b
Anthony Lin <Anthony@Anthonys-MacBook-Air.local>
2013-06-18 10:30:16 -0400

    Add conversion file

---
+++ IdeaProjects/Constants/src/ConstantsRate.java
@@ -0,0 +1,16 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Anthony
+ * Date: 13-06-18
+ * Time: 10:23 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class ConstantsRate {
+    // Conversion is from left unit to right unit
+    public int FahrenheitCelcius(int f) {
+        int c = f - 32 * 5 / 9;
+        return c;
+
+    }
+
+}
